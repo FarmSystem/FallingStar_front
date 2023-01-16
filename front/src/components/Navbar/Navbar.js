66551f0f7eeebd795afe72471cf1909522c645d6
Eunse Jang <senuej37@gmai.com>
2023-01-16 20:29:01 +0900

    SignIn + Lawyer 페이지

--- front/src/components/Navbar/Navbar.js
+++ front/src/components/Navbar/Navbar.js
@@ -1,198 +1,207 @@
-import React from 'react';
-import styled from 'styled-components';
-import HomeIcon from '@mui/icons-material/Home';
-import ModeEditIcon from '@mui/icons-material/ModeEdit';
-import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
-import ContactMailIcon from '@mui/icons-material/ContactMail';
-import TextsmsIcon from '@mui/icons-material/Textsms';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import logoTitle2 from '../../assets/img/LogoTitle.png';
+import React from "react";
+import styled from "styled-components";
+import HomeIcon from "@mui/icons-material/Home";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
+import TextsmsIcon from "@mui/icons-material/Textsms";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import logoTitle2 from "../../assets/img/LogoTitle.png";
 
 // ㅇㅇ
 const Wrapper = styled.div`
-    position: absolute;
-    padding: 0
-    margin: 0
-    top: 0;
-    width: 100%;
-    height: 100px;
-    display: flex;
-    flex-direction: column;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
 `;
 
 // ㅇㅇ
 const LogoContainer = styled.div`
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 50px;
-    background: #1E1E1E;  
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    justify-content: flex-end;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 50px;
+  background: #1e1e1e;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  justify-content: flex-end;
 `;
 
 const Logo = styled.div`
-    width: 45%;
-    display: flex;
-    flex-direction: row;
+  width: 45%;
+  display: flex;
+  flex-direction: row;
 `;
 
 const LogoTitle = styled.div`
-
-    padding: 12px 0 12px 5px;
-    width: 120x;
-    font-family: 'JejuMyeongjo';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 28px;
-    color: #FFFFFF;
+  padding: 12px 0 12px 5px;
+  width: 120x;
+  font-family: "JejuMyeongjo";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 28px;
+  color: #ffffff;
+  cursor: pointer;
 `;
 
 const LogoDetailText = styled.div`
-    padding: 2px 0 0 0;
-    font-family: 'JejuMyeongjo';
-    font-style: normal;
-    ont-weight: 400;
-    font-size: 10px;
-    color: white;
+  padding: 2px 0 0 0;
+  font-family: "JejuMyeongjo";
+  font-style: normal;
+  ont-weight: 400;
+  font-size: 10px;
+  color: white;
 `;
 
 const LogoDetail = styled.div`
-    padding: 12px 5px 12px 12px;
-    text-align: right;
-    font-family: 'JejuMyeongjo';
-    font-style: normal;
-    ont-weight: 400;
-    font-size: 10px;
-    color: white;
+  padding: 12px 5px 12px 12px;
+  text-align: right;
+  font-family: "JejuMyeongjo";
+  font-style: normal;
+  ont-weight: 400;
+  font-size: 10px;
+  color: white;
+  cursor: pointer;
 `;
 
 const LogoMenu = styled.div`
-    display: flex;
-    justify-content: space-around; 
-    padding: 12px;
-    width: 180px;
-    color: white;
+  display: flex;
+  justify-content: space-around;
+  padding: 12px;
+  width: 180px;
+  color: white;
 `;
 
 const LogoMenuText = styled.div`
-    padding: 2px 0 0 0;
-    height: 20px;
-    font-family: 'JejuMyeongjo';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 18px;
-    color: white;
-    cursor: pointer;
+  padding: 2px 0 0 0;
+  height: 20px;
+  font-family: "JejuMyeongjo";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  color: white;
+  cursor: pointer;
 `;
 
 //background: rgba(254, 254, 254, 0.5);
 // ㅇㅇ
 const NavContainer = styled.div`
-    position:absolute;
-    top: 50px;
-    width: 100%;
-    height: 50px;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-    background: grey;
+  position: absolute;
+  left: 0;
+  top: 50px;
+  width: 100%;
+  height: 50px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  background: grey;
 `;
 
-
 const Nav = styled.div`
-    ul {
+  ul {
     display: flex;
     justify-content: space-around;
     list-style: none;
     align-items: center;
-}
-    
-    li {
+  }
+
+  li {
     display: flex;
     flex-direction: row;
-    font-family: 'JejuMyeongjo';
+    font-family: "JejuMyeongjo";
     cursor: pointer;
     color: white;
     font-style: normal;
     font-weight: 400;
     text-align: center;
-}
+  }
 `;
 
 const MenuText = styled.div`
-    padding-top: 6px;
-    padding-left: 7px;
-    font-family: 'JejuMyeongjo';
-    cursor: pointer;
-    color: white;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 14px;
-    text-align: center;
+  padding-top: 6px;
+  padding-left: 7px;
+  font-family: "JejuMyeongjo";
+  cursor: pointer;
+  color: white;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  text-align: center;
 `;
 
 const LogoTitle2 = styled.img`
-    display: flex;
-    padding: 13px 0 13px 0;
-    width: 62px;
-    height: 26px;
-    align-item: center;
+  display: flex;
+  padding: 13px 0 13px 0;
+  width: 62px;
+  height: 26px;
+  align-item: center;
+  cursor: pointer;
 `;
 
 const Navbar = () => {
-    return (
-        <Wrapper>
-            <LogoContainer>
-                <Logo>
-                    <LogoDetail>
-                        <LogoDetailText>블록체인 유언장</LogoDetailText>
-                        <LogoDetailText>법률 커뮤니티</LogoDetailText>
-                    </LogoDetail>
-                    <LogoTitle>별세</LogoTitle>
-                    <LogoTitle2 src={logoTitle2} />
-                </Logo>
+  return (
+    <Wrapper>
+      <LogoContainer>
+        <Logo>
+          <LogoDetail onClick={() => (window.location.href = "./")}>
+            <LogoDetailText>블록체인 유언장</LogoDetailText>
+            <LogoDetailText>법률 커뮤니티</LogoDetailText>
+          </LogoDetail>
+          <LogoTitle onClick={() => (window.location.href = "./")}>
+            별세
+          </LogoTitle>
+          <LogoTitle2
+            onClick={() => (window.location.href = "./")}
+            src={logoTitle2}
+          />
+        </Logo>
 
-                <LogoMenu>
-                    <AccountCircleIcon />
-                    <LogoMenuText onClick={() => (window.location.href = './SignIn')}>
-                        로그인</LogoMenuText>
-                    <LogoMenuText>/</LogoMenuText>
-                    <LogoMenuText onClick={() => (window.location.href = './SignUp')}>
-                        회원가입</LogoMenuText>
-                </LogoMenu>
-            </LogoContainer>
+        <LogoMenu>
+          <AccountCircleIcon />
+          <LogoMenuText onClick={() => (window.location.href = "./SignIn")}>
+            로그인
+          </LogoMenuText>
+          <LogoMenuText>/</LogoMenuText>
+          <LogoMenuText onClick={() => (window.location.href = "./SignUp")}>
+            회원가입
+          </LogoMenuText>
+        </LogoMenu>
+      </LogoContainer>
 
+      <NavContainer>
+        <Nav>
+          <ul>
+            <li onClick={() => (window.location.href = "./introduce")}>
+              <HomeIcon />
+              <MenuText>소개페이지</MenuText>
+            </li>
 
-            <NavContainer>
-                <Nav>
-                    <ul>
-                        <li onClick={() => (window.location.href = './introduce')}>
-                            <HomeIcon /><MenuText>소개페이지</MenuText>
-                        </li>
+            <li onClick={() => (window.location.href = "./WillWriting_notice")}>
+              <ModeEditIcon />
+              <MenuText>유언장 작성하기</MenuText>
+            </li>
+            <li onClick={() => (window.location.href = "./WillSearch")}>
+              <DescriptionOutlinedIcon />
+              <MenuText>유언장 찾기</MenuText>
+            </li>
+            <li onClick={() => (window.location.href = "./Lawyer")}>
+              <ContactMailIcon />
+              <MenuText>변호사 찾기</MenuText>
+            </li>
+            <li onClick={() => (window.location.href = "./Question")}>
+              <TextsmsIcon />
+              <MenuText>법률 게시판</MenuText>
+            </li>
+          </ul>
+        </Nav>
+      </NavContainer>
+    </Wrapper>
 
-                        <li onClick={() => (window.location.href = './WillWriting_notice')}>
-                            <ModeEditIcon /><MenuText>유언장 작성하기</MenuText>
-                        </li>
-                        <li onClick={() => (window.location.href = './WillSearch')}>
-                            <DescriptionOutlinedIcon />
-                            <MenuText>유언장 찾기</MenuText>
-                        </li>
-                        <li onClick={() => (window.location.href = './Lawyer')}>
-                            <ContactMailIcon />
-                            <MenuText>변호사 찾기</MenuText>
-                        </li>
-                        <li onClick={() => (window.location.href = './Question')}>
-                            <TextsmsIcon />
-                            <MenuText>법률 게시판</MenuText>
-                        </li>
-                    </ul>
-                </Nav>
-            </NavContainer>
-        </Wrapper>
-
-        /*
+    /*
         <>
             <Container>
                 내브바
@@ -204,7 +213,7 @@ const Navbar = () => {
             </Container>
         </>
         */
-    )
-}
+  );
+};
 
 export default Navbar;
--- front/src/pages/Lawyer/Lawyer_profile.js
+++ front/src/pages/Lawyer/Lawyer_profile.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+import lawyerProfile from "../../assets/img/lawyerProfile.png";
+import lawyerBackgroundImg from "../../assets/img/lawyerBackgroundImg.png";
+import { symbol } from "prop-types";
+
+const Background = styled.body`
+background-color: #F5F5F5;
+padding-bottom: 100px;
+min-height:100%
+height: auto;
+`;
+
+const LawyerBackgroundImg = styled.img`
+  width: 100%;
+  height: 400px;
+  opacity: 0.2;
+  margin: 98px 0 0 0;
+`;
+
+const ProfileContainer = styled.div`
+  border: 1px solid red;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const LawyerProfile = styled.img`
+  width: 1000px;
+`;
 
 function Lawyer_profile() {
-    return (
-        <>
-        변호사 프로필
-        </>
-    )
+  return (
+    <Background>
+      <LawyerProfile src={lawyerProfile} />
+      <LawyerBackgroundImg src={lawyerBackgroundImg} />
+    </Background>
+  );
 }
 
-export default Lawyer_profile
+export default Lawyer_profile;
