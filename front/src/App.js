628b7c74828c105af99d4f0dbd89194b65833e94
Eunse Jang <senuej37@gmai.com>
2023-01-16 10:57:08 +0900

    SignUp 페이지 구현

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,7 @@
 import GlobalStyle from "./styles/GlobalStyle";
-import { Routes, Route,Outlet, useLocation } from 'react-router-dom';
-import {useState} from 'react';
-import {useSelector, useDispatch} from "react-redux"
-
+import { Routes, Route, Outlet, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 //contextAPI
 import { ColorContext } from "./context/ColorContext";
@@ -14,7 +13,8 @@ import Mainpage from "./pages/Mainpage"; //메인페이지
 import Introduce from "./pages/Introduce"; //소개페이지
 //유언장 검색
 import WillSearch from "./pages/WillSearch/WillSearch"; //유언장검색 첫페이지
-import WillSearch_find from "./pages/WillSearch/WillSearch_find"; //유언장 검색결과 있을때 페이지
+import WillSearch_find1 from "./pages/WillSearch/WillSearch_find1"; //유언장 검색결과 있을때 페이지
+import WillSearch_find2 from "./pages/WillSearch/WillSearch_find2"; //유언장 검색결과 있을때 페이지
 import WillSearch_nonefind from "./pages/WillSearch/WillSearch_nonefind"; //유언장 검색결과 없을때 페이지
 import WillSearch_complete from "./pages/WillSearch/WillSearch_complete"; //유언장 검색에서 찾기누르면 페이지
 //유언장 작성
@@ -39,51 +39,60 @@ import Mypage from "./pages/User/MyPage";
 import SignIn from "./pages/User/SignIn";
 import SignUp from "./pages/User/SignUp";
 
-const Layout = ()=>{
+const Layout = () => {
   return (
     <>
-    <Navbar />
-    <Outlet />
-    <Footer />
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 function App() {
   return (
     <>
-    <GlobalStyle />
-    
-    <Routes>
-    <Route path="/" element={<Layout />}>  //상위 라우터 요걸해줘야 레이아웃 적용됨
-        <Route index element={<Mainpage />} />
-        <Route path='introduce' element={<Introduce />} />
-        
-        <Route path='WillSearch' element={<WillSearch />} />
-        <Route path='WillSearch_find' element={<WillSearch_find />} />
-        <Route path='WillSearch_nonefind' element={<WillSearch_nonefind />} />
-        <Route path='WillSearch_complete' element={<WillSearch_complete />} />
-        
-        <Route path='WillWriting_notice' element={<WillWriting_notice />} />
-        <Route path='WillWriting_step1' element={<WillWriting_step1 />} />
-        <Route path='WillWriting_step2' element={<WillWriting_step2 />}/>
-        <Route path='WillWriting_step3' element={<WillWriting_step3 />}/>
-        <Route path='WillWriting_step4' element={<WillWriting_step4 />}/>
-        <Route path='WillWriting_step5' element={<WillWriting_step5 />}/>
-        <Route path='WillWriting_recording' element={<WillWriting_recording />}/>
-        <Route path='WillWriting_opener' element={<WillWriting_opener />}/>
-        <Route path='WillWriting_complete1' element={<WillWriting_complete1 />}/>
-        <Route path='WillWriting_complete2' element={<WillWriting_complete2 />}/>
+      <GlobalStyle />
 
-        <Route path='Lawyer' element={<Lawyer />}/>
-        <Route path='Lawyer_profile' element={<Lawyer_profile />}/>
-        <Route path='Question' element={<Question />}/>
-        <Route path='Question_detail' element={<Question_detail/>}/>
-        <Route path='Mypage' element={<Mypage />}/>
-        <Route path='SignUp' element={<SignUp />}/>
-        <Route path='SignIn' element={<SignIn />}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {" "}
+          //상위 라우터 요걸해줘야 레이아웃 적용됨
+          <Route index element={<Mainpage />} />
+          <Route path="introduce" element={<Introduce />} />
+          <Route path="WillSearch" element={<WillSearch />} />
+          <Route path="WillSearch_find1" element={<WillSearch_find1 />} />
+          <Route path="WillSearch_find2" element={<WillSearch_find2 />} />
+          <Route path="WillSearch_nonefind" element={<WillSearch_nonefind />} />
+          <Route path="WillSearch_complete" element={<WillSearch_complete />} />
+          <Route path="WillWriting_notice" element={<WillWriting_notice />} />
+          <Route path="WillWriting_step1" element={<WillWriting_step1 />} />
+          <Route path="WillWriting_step2" element={<WillWriting_step2 />} />
+          <Route path="WillWriting_step3" element={<WillWriting_step3 />} />
+          <Route path="WillWriting_step4" element={<WillWriting_step4 />} />
+          <Route path="WillWriting_step5" element={<WillWriting_step5 />} />
+          <Route
+            path="WillWriting_recording"
+            element={<WillWriting_recording />}
+          />
+          <Route path="WillWriting_opener" element={<WillWriting_opener />} />
+          <Route
+            path="WillWriting_complete1"
+            element={<WillWriting_complete1 />}
+          />
+          <Route
+            path="WillWriting_complete2"
+            element={<WillWriting_complete2 />}
+          />
+          <Route path="Lawyer" element={<Lawyer />} />
+          <Route path="Lawyer_profile" element={<Lawyer_profile />} />
+          <Route path="Question" element={<Question />} />
+          <Route path="Question_detail" element={<Question_detail />} />
+          <Route path="Mypage" element={<Mypage />} />
+          <Route path="SignUp" element={<SignUp />} />
+          <Route path="SignIn" element={<SignIn />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- front/src/pages/WillSearch/WillSearch_find1.js
+++ front/src/pages/WillSearch/WillSearch_find1.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import searchLogo from "../../assets/img/SearchLogo.png";
-import completeImg from "../../assets/img/CompleteImg.png";
+import writingFindImg from "../../assets/img/WritingFindImg.png";
 
 const Background = styled.body`
 background-color: #E7E5E0;
 padding-bottom: 100px;
 min-height:100%
 height: auto;
+li {
+    list-style: none;
+}
 `;
 
 const Container = styled.div`
@@ -40,6 +43,19 @@ const FindText = styled.div`
   z-index: 1;
 `;
 
+const DateText = styled.div`
+  position: absolute;
+  top: 840px;
+  width: 100%;
+  text-align: center;
+
+  font-family: "JejuMyeongjo";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 17px;
+  z-index: 1;
+`;
+
 const WritingFindImg = styled.img`
   width: 650px;
 `;
@@ -48,48 +64,35 @@ const SearchLogo = styled.img`
   width: 400px;
 `;
 
-const CompleteImg = styled.img`
-  width: 450px;
-`;
-
 const ButtonContainer = styled.div`
   width: 100%;
   align-items: center;
   display: flex;
   flex-direction: column;
-  height: 90px;
+
+  width: 770px;
+  height: 80px;
 `;
 
 const Button = styled.button`
   font-family: "Inter";
   font-style: normal;
-  font-weight: 600;
-  font-size: 17px;
+  font-weight: 900;
+  font-size: 20px;
   line-height: 18px;
   text-align: center;
 
-  color: #ffffff;
+  color: #000000;
 
-  border: 1px solid #585858;
-  background: #585858;
+  border: 1px solid #ffffff;
+  background: #ffffff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 5px;
+  border-radius: 6px;
   padding: 7px;
   margin: 12px;
-  width: 180px;
+  width: 150px;
   height: 50px;
   cursor: pointer;
-`;
-
-const NoticeText = styled.div`
-  font-family: "Inter";
-  font-style: normal;
-  font-weight: 800;
-  font-size: 16px;
-  line-height: 24px;
-  text-align: center;
-
-  color: #8d8d8d;
 `;
 
 function WillSearch() {
@@ -122,18 +125,14 @@ function WillSearch() {
         <br />
         <br />
         <br />
-        <br />
-        <CompleteImg src={completeImg} />
-        <br />
+        <WritingFindImg src={writingFindImg} />
+        <DateText>작성 날짜</DateText>
+        <br /> <br /> <br />
         <ButtonContainer>
-          <Button onClick={() => (window.location.href = "/MyPage")}>
-            마이페이지 가기
+          <Button onClick={() => (window.location.href = "/WillSearch_find2")}>
+            찾기
           </Button>
         </ButtonContainer>
-        <NoticeText>
-          관계인 승인 후, 이메일로 유언장 사용과 관련한 <br />
-          추가적인 서류 절차 안내 이메일이 발송됩니다.
-        </NoticeText>
       </Container>
     </Background>
   );
