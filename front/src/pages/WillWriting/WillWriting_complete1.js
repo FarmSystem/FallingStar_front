b742faff63dbd2bbc6e8a675bc79c1f8191b38f7
lywon725 <lywon725@naver.com>
2023-01-20 21:02:34 +0900

    유언장 작성 페이지 완성
    녹음 추가
    타이핑 애니메이션

--- front/src/pages/WillWriting/WillWriting_complete1.js
+++ front/src/pages/WillWriting/WillWriting_complete1.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
+import Typed from "react-typed"
 import InviteBox from '../../components/Invite/Invite';
 import * as WCss from '../../styles/WillWritinCss';
 //리덕스
@@ -9,24 +9,102 @@ import { Question1, Question2, Question3 } from '../../redux/store';
 
 //이미지 
 import WillWritingTitle from '../../assets/img/WillWritingTitle.png';
-import stepImg from '../../assets/img/WillWriting_opener.png';
+import blockcheinIcon from '../../assets/img/blockcheinIcon.png';
+import check from '../../assets/img/check.png';
 
-export const Background = styled.body`
-background-color: #E9EAEC;
+const Background = styled.body`
+background: linear-gradient(170.32deg, #FFF2E3 7.81%, #E9EAEC 87.61%);
 padding-bottom: 100px;
 padding-top: 200px;
 min-height:100%
 height: auto;
 text-align: center;
 `
+const Text = styled.div`
+font-family: 'Inter';
+font-style: normal;
+font-weight: 700;
+font-size: 35px;
+line-height: 42px;
+text-align: center;
+
+color: #000000;
+display: inline-block;
+
+`
+const Typing = styled.div`
+display: inline;
+font-family: 'Inter';
+font-style: normal;
+font-weight: 700;
+font-size: 35px;
+line-height: 42px;
+text-align: center;
+
+color: #000000;
+`
+
+const Img2 = styled.img`
+width: 100px;
+margin: 20px;
+`
+const SubText = styled.div`
+font-family: 'Inter';
+font-style: normal;
+font-weight: 500;
+font-size: 23px;
+line-height: 34px;
+text-align: center;
+
+color: #4A4A4A;
+`
+const PointSubText = styled.div`
+font-family: 'Inter';
+font-style: normal;
+font-weight: 500;
+font-size: 23px;
+line-height: 28px;
+text-align: center;
+white-space: nowrap;
+display: inline;
+color: #E0AF00;
+`
+
 function WillWriting_complete1() {
+    
+    const content = "...ddddd";
+    const [show, setShow] = useState(false);
+    const wait = setTimeout(() => {
+        setShow(true);
+        clearTimeout(wait); //메모리를 위해 타이머 해제
+    }, 4000) //3초
     return (
         <Background>
             <WCss.Container>
                 <WCss.Img src={WillWritingTitle} />
-\                
+                {show==true? <>
+                    <Typing>
+                        <Text>유언장 작성이 완료되었습니다.</Text>                 
+                        <Typed strings={["..."]} typeSpeed={200} backSpeed={200} loop />
+                    </Typing>
+                    <br  />
+                    <Img2 src={check}/>
+                    <SubText>작성하신 <PointSubText>마이페이지</PointSubText>
+                        에서 <br/>학인 및 수정하실 수 있습니다.
+                    </SubText>
+                </>:<>
+                    <Typing>
+                        <Text>블록체인 유언장을 생성합니다</Text>                 
+                        <Typed strings={["..."]} typeSpeed={200} backSpeed={200} loop />
+                    </Typing>
+                    <br  />
+                    <Img2 src={blockcheinIcon}/>
+                    <SubText>작성된 유언장은 <PointSubText>블록체인 기술</PointSubText>
+                        을 통해 <br/>타인에 의해 훼손되지 않고 안전하게 보관됩니다.
+                    </SubText>
+                </>}
+                
             </WCss.Container>
-
         </Background>
         
     )
--- front/src/pages/WillWriting/WillWriting_recording.js
+++ front/src/pages/WillWriting/WillWriting_recording.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 
 import InviteBox from '../../components/Invite/Invite';
@@ -18,37 +18,71 @@ const Text = styled.div`
 font-family: 'Inter';
 font-style: normal;
 font-weight: 800;
-font-size: 20px;
-line-height: 30px;
+font-size: 26px;
+line-height: 38px;
 padding-bottom: 10px;
 text-align: center;
 color: #000000;
 `
+const SmallText = styled.div`
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 13px;
+line-height: 20px;
+padding-bottom: 10px;
+text-align: center;
+color: grey;
+`
 const RecordButton = styled.button`
 width: 100px;
+background-color: white;
 height: 100px;
 border-radius: 200px;
 padding: 0;
 border: 2px solid #FFB762;
 filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+margin: 20px;
+font-size: 16px;
+font-weight: 800;
+`
+const RecordOnButton = styled.button`
+width: 100px;
+background-color: white;
+height: 100px;
+border-radius: 200px;
+padding: 0;
+border: 2px solid red;
+filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+margin: 20px;
 
-
+font-size: 16px;
+font-weight: 800;
 `
 const RecordImg = styled.img`
-width: 100px;
+width: 30px;
 padding: 0;
+margin-bottom: 5px;
+`
+const Play = styled.button`
+padding: 10;
+width: 100px;
+height: 30px;
+border-radius: 10px;
+background-color: orange;
 `
 const WillBox = styled.div`
-width: 80%;
-background: #EDEAE0;
+width: 60%;
 border-radius: 10px;
+border: 0.1px solid grey;
 margin: 20px;
-margin-bottom: 40px;
-padding: 20px;
-padding-left: 40px;
-padding-right: 40px;
+margin-bottom: 10px;
+padding: 30px;
+padding-left: 80px;
+padding-right: 80px;
 display: inline-block;
-
+background-image: url('../../assets/img/TextAreaImg.png');
+font-family: 'JejuMyeongjo';
 text-align: left;
 line-height:24px;
 `
@@ -60,7 +94,8 @@ const SubRecordText = styled.div`
 `
 const Br = styled.div`
 margin: 10px;
-`
+// `
+
 function WillWriting_recording() {
     //리덕스
     let a = useSelector((state) => { return state } )
@@ -73,6 +108,106 @@ function WillWriting_recording() {
     let month = now.getMonth()+1; //월
     let day = now.getDate(); //일
 
+    //녹음
+    const [stream, setStream] = useState();
+    const [media, setMedia] = useState();
+    const [onRec, setOnRec] = useState(true);
+    const [source, setSource] = useState();
+    const [analyser, setAnalyser] = useState();
+    const [audioUrl, setAudioUrl] = useState();
+    const [disabled, setDisabled] = useState(true); 
+    // //타이머
+    // const Hours = Timer.Hours;
+    // const Minutes = Timer.Minutes;
+    // const Seconds = Timer.Seconds;
+    const onRecAudio = () => {
+
+        setDisabled(true) // 😀😀😀
+        
+        // 음원정보를 담은 노드를 생성하거나 음원을 실행또는 디코딩 시키는 일을 한다
+        const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        // 자바스크립트를 통해 음원의 진행상태에 직접접근에 사용된다.
+        const analyser = audioCtx.createScriptProcessor(0, 1, 1);
+        setAnalyser(analyser);
+    
+        function makeSound(stream) {
+          // 내 컴퓨터의 마이크나 다른 소스를 통해 발생한 오디오 스트림의 정보를 보여준다.
+            const source = audioCtx.createMediaStreamSource(stream);
+            setSource(source);
+            source.connect(analyser);
+            analyser.connect(audioCtx.destination);
+        }
+        // 마이크 사용 권한 획득
+        navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+            const mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start();
+            setStream(stream);
+            setMedia(mediaRecorder);
+            makeSound(stream);
+    
+            analyser.onaudioprocess = function (e) {
+                // 3분(180초) 지나면 자동으로 음성 저장 및 녹음 중지
+                if (e.playbackTime > 1000) {
+                stream.getAudioTracks().forEach(function (track) {
+                    track.stop();
+                });
+                mediaRecorder.stop();
+                // 메서드가 호출 된 노드 연결 해제
+                analyser.disconnect();
+                audioCtx.createMediaStreamSource(stream).disconnect();
+    
+                mediaRecorder.ondataavailable = function (e) {
+                    setAudioUrl(e.data);
+                    setOnRec(true);
+                };
+                } else {
+                setOnRec(false);
+                }
+            };
+            });
+    };
+    
+    // 사용자가 음성 녹음을 중지 했을 때
+    const offRecAudio = () => {
+        // dataavailable 이벤트로 Blob 데이터에 대한 응답을 받을 수 있음
+        media.ondataavailable = function (e) {
+            setAudioUrl(e.data);
+            setOnRec(true);
+    };
+    
+    // 모든 트랙에서 stop()을 호출해 오디오 스트림을 정지
+    stream.getAudioTracks().forEach(function (track) {
+        track.stop();
+    });
+    
+        // 미디어 캡처 중지
+        media.stop();
+    
+        // 메서드가 호출 된 노드 연결 해제
+        analyser.disconnect();
+        source.disconnect();
+        
+        if (audioUrl) {
+          URL.createObjectURL(audioUrl); // 출력된 링크에서 녹음된 오디오 확인 가능
+        }
+        
+        // File 생성자를 사용해 파일로 변환
+        const sound = new File([audioUrl], "soundBlob", {
+            lastModified: new Date().getTime(),
+            type: "audio",
+        });
+    
+        // 😀😀😀
+        setDisabled(false);
+        console.log(sound); // File 정보 출력
+    };
+    
+    const play = () => { 
+        const audio = new Audio(URL.createObjectURL(audioUrl)); // 😀😀😀
+        audio.loop = false;
+        audio.volume = 1;
+        audio.play();
+    };
     return (
         <WCss.Background>
             {console.log(a)}
@@ -84,10 +219,10 @@ function WillWriting_recording() {
                     <WCss.Line/>
                     <WCss.SmallText>법적효력이 있는 유언장을 저장하기 위해 <br/>작성해주신 유언장 내용을 바탕으로 녹음을 진행합니다.</WCss.SmallText>
                     <WCss.ButtonImg><WCss.Img2 src = {notice}/></WCss.ButtonImg>
-                    <br />
+                    <br /><br />
                     <Text>작성된 유언장 내용을 확인해주세요.</Text>
-                    <WCss.SmallText>작성해주신 유언장 작성 1,2,3,4단계의 내용을 기반을 작성된 유언장입니다. <br/> 
-                    유언장 작성하기 5단계의 내용은 마이페이지 유언장 보관함에서 확인하실 수 있습니다.</WCss.SmallText>
+                    <SmallText>작성해주신 유언장 작성 1,2,3,4단계의 내용을 기반을 작성된 유언장입니다. <br/> 
+                    유언장 작성하기 5단계의 유언장 내용은 마이페이지에서 확인하실 수 있습니다.</SmallText>
                     <WillBox>
                         {(a.login_user.birth).substr(0, 2)} 년 {(a.login_user.birth).substr(2, 2)} 월 {(a.login_user.birth).substr(4, 2)} 일생 <br />
                         유언자 {a.login_user.name}
@@ -128,21 +263,32 @@ function WillWriting_recording() {
 
                         <br/><br/>작성일자 :  {year}년 {month}월 {day}일 
                     </WillBox>
+                    <SmallText>유언장 내용 수정은 페이지 하단의 '이전으로' 버튼을 통해 수정이 가능합니다.</SmallText>
+                    <br/> <br/> <br/> 
                     <div/>
                     <Text> 유언장 내용을 확인하셨다면,<br/> 유언장 내용을 읽으며 녹음을 진행해주세요. <br/></Text> 
-                    <WCss.SmallText>유언장 내용 수정은 페이지 하단의 '이전으로' 버튼을 통해 수정이 가능합니다.</WCss.SmallText>
-                    <br/> <br/> 
-                    <Text> 아래 녹음 버튼을 클릭하시면 녹음이 진행됩니다. </Text>
-                    {/* 버튼 if문 */}
-                    {click==true ? <>
-                        <RecordButton><RecordImg src ={Record}/></RecordButton>
-                        {/* 녹음진행 프로그레스 바 */}
-                        <RecordText>녹음 중입니다..</RecordText>
-                        <SubRecordText>녹음을 중지하려면 버튼을 클릭하세요.</SubRecordText>
-                    </>:<>
-                        <RecordButton><RecordImg src ={Record}/></RecordButton>
 
+                    
+                    {/* 버튼 if문 */}
+                    {onRec ==true ? <>
+                        {/* 녹음진행 x */}
+                        <WCss.SmallText>녹음 버튼을 클릭하시면 녹음이 진행됩니다. <br/> <br/> </WCss.SmallText>
+                        <RecordButton onClick={onRec ? onRecAudio : offRecAudio}> 
+                            <RecordImg src ={Record}/> <br/>녹음
+                        </RecordButton>
+                        <br/><br/><br/>
+                        <Play onClick={play} disabled={disabled}> 녹음 확인</Play>
+
+                    </>:<>
+                        {/* 녹음진행 중 */}
+                        <WCss.SmallText>녹음 중입니다..</WCss.SmallText>
+                        <WCss.SmallText>녹음을 중지하려면 녹음버튼을 클릭하세요.</WCss.SmallText>
+                        <RecordOnButton onClick={onRec ? onRecAudio : offRecAudio}> 
+                            <RecordImg src ={Record}/><br/>중지
+                        </RecordOnButton>
                     </>}
+
+
                 </WCss.Box>
                 <WCss.ButtonContainer>
                     <WCss.PrevButton onClick={() => (window.location.href = '/WillWriting_step5')}>
